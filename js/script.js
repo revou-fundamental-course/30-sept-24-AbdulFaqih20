435ac88f333a8e72539b2df1d882e3baff46e59d
AbdulFaqih20 <faqihabdul517@gmail.com>
2024-10-09 23:04:32 +0700

    konfigurasi akhir

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-// ini js
+function convert() {
+    const celcius = document.getElementById("celcius").value;
+    if (celcius === "") {
+      document.getElementById("result").textContent =
+        "Harap masukkan suhu.";
+      return;
+    }
+    const fahrenheit = (celcius * 9) / 5 + 32;
+    document.getElementById(
+      "result"
+    ).textContent = `Hasil:= ${fahrenheit.toFixed(2)}°F`;
+    document.getElementById(
+      "calculation"
+    ).textContent = `Rumus: (${celcius} × 9/5) + 32 = ${fahrenheit.toFixed(
+      2
+    )}°F`;
+  }
+  function resetToZero() {
+    document.getElementById("suhuForm").reset();
+    document.getElementById("result").textContent = "";
+    document.getElementById("calculation").textContent = "";
+  }
+  function reverse() {
+    const fahrenheit = document.getElementById("celcius").value;
+    if (fahrenheit === "") {
+      document.getElementById("result").textContent =
+        "Harap masukkan suhu.";
+      return;
+    }
+    const celsius = ((fahrenheit - 32) * 5) / 9;
+    document.getElementById(
+      "result"
+    ).textContent = `Hasil: ${celsius.toFixed(2)}°C`;
+    document.getElementById(
+      "calculation"
+    ).textContent = `Rumus: (${fahrenheit} - 32) × 5/9 = ${celsius.toFixed(
+      2
+    )}°C`;
+  }
